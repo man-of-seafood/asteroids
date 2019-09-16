65bd63e2341ba70d0dd9349aede59df2f9862769
Mason Raasch <raasch.mason@gmail.com>
2019-09-15 20:54:05 -0700

    uh

--- graphics/position.js
+++
@@ -1,8 +0,0 @@
-class Position {
-  constructor(initX = 0 , initY = 0) {
-    this.x = initX
-    this.y = initY
-  }
-}
-
-export default Position;
--- graphics/velocity.js
+++
@@ -1,8 +0,0 @@
-class Velocity {
-  constructor(initSpeed = 1, initAngle = 0) {
-    this.speed = initSpeed; // something like px/s
-    this.angle = initAngle; // something like a compass angle
-  }
-}
-
-export default Velocity;
